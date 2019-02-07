63c8dd5387d56dc4f3e0cbe6fb29651e5af7ef62
FELIPEACARNEIRO <47108323+FELIPEACARNEIRO@users.noreply.github.com>
2019-02-07 15:15:30 -0200

    criação do arquivo main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package goarea
+
+import "math"
+
+const P1= 3.1416
+
+func Circ(raio float64){
+return math.Pow(raio,2)*P1
+}
+
+func Rect(base , altura gloat64) float64{
+return base*altura
+}
+
+func _TrinaguloEq(base,altura float) float64{
+return (base=altura) / 2
+}
