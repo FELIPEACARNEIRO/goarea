0c35d7351d070196ab2ff5fd12b7b12211280f9c
FELIPEACARNEIRO <47108323+FELIPEACARNEIRO@users.noreply.github.com>
2019-02-07 15:20:56 -0200

    Update main.go

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package goarea
 
 import "math"
 
-const P1= 3.1416
+const Pi= 3.1416
 
 func Circ(raio float64){
-return math.Pow(raio,2)*P1
+      return math.Pow(raio,2)*Pi
 }
 
 func Rect(base , altura gloat64) float64{
-return base*altura
+      return base * altura
 }
 
 func _TrinaguloEq(base,altura float) float64{
-return (bas*altura) / 2
+      return (bas*altura) / 2
 }
